2419d8551370366f709d98582a7887bee11d5166
Lucas Dias <lucasdribeiro252@gmail.com>
2022-04-16 01:06:44 -0300

    Implementacao da arvore de decisao ID3

--- ID3_tree_implementation.py
+++ ID3_tree_implementation.py
@@ -1,3 +1,4 @@
+import concurrent.futures
 import ID3_functions as id3f
 
 class Tree():
@@ -6,41 +7,41 @@ class Tree():
     def __init__(self,name = None):
         self.name = name
         self.tree = {}
-        self.first_atribute = 0
+        self.first_category = 0
 
     #Função auxiliar para percorrer a arvore e chegar ate o nó definido pela lista
-    def __get_node(self,path):
+    def __get_node(self,path,tree):
 
         #Selecionar o primeiro flho do dicionario
-        node = self.tree.get(path[0])
+        node = tree.get(path[0])
 
         #Selecionar os demais filhos que constam em list
         for _ in range(len(path)-1):
             node = node.get(path[_+1])
     
         return node
-    
+
     #Definir a função que a partir de um atributo decide qual o proximo que deve ser avaliado
-    def __chose_next_node(self,list_categories,path,df):
+    def __chose_next_node(self,list_categories,path,df,tree):
 
         #Escolher um dataframe que contenha só as linhas que correspondem a categoria indicada em path
-        for _,df_principal in df.groupby(path[-1]):
+        for attribute,df_principal in df.groupby(path[-1]):
 
             #Definir entropia inicial desse nó:
             inicial_entropy = df_principal.Rating.value_counts(normalize = True).map(lambda x: -x*id3f.log(x)).sum()
 
             #Definir a entropia para cada categoria disponivel
             entropy_gain = {}
-            for attribute in (set(list_categories) - set(path)):
+            for category in (set(list_categories) - set(path)):
                 total = 0
                 soma = 0
 
-                for p,df_subselected in df_principal.groupby(attribute):
+                for p,df_subselected in df_principal.groupby(category):
                     attribute_column = df_subselected.Rating
                     total += attribute_column.count()
                     soma += (attribute_column.value_counts(normalize = True).map(lambda x: -x*id3f.log(x)).sum())*attribute_column.count()
-                entropy_gain[attribute] = inicial_entropy - soma/total
-            
+                entropy_gain[category] = inicial_entropy - soma/total
+
             #Selecionar o maior ganho de entropia
             category,value = sorted(entropy_gain.items(), key = lambda item: item[1], reverse = True)[0]
 
@@ -48,24 +49,31 @@ class Tree():
             if value > 0.01:
 
                 #Adicionar o valor 
-                self.__get_node(path)[_] = {}
-                path.append(_)
+                self.__get_node(path,tree)[attribute] = {}
+                path.append(attribute)
 
                 #Adicionar categoria escolhida
-                self.__get_node(path)[category] = {}
+                self.__get_node(path,tree)[category] = {}
                 path.append(category)
 
                 #escolher proxima categoria
-                self.__chose_next_node(list_categories,path,df_principal)
+                self.__chose_next_node(list_categories,path,df_principal,tree)
 
                 #Retirar os parametros de profundidade do nó
                 path.pop()
                 path.pop()
 
-            #Caso não exista ganho de entropia
+            #Caso não exista ganho de entropia, escolher a nota que sera dada
             else:
-                self.__get_node(path)[_] = df_principal.Rating.value_counts().index[0]
-            
+                self.__get_node(path,tree)[attribute] = df_principal.Rating.value_counts().index[0]
+                
+
+    #Função para definir a arvore
+    def define_tree(self,list_categories,path,df_aux):
+        tree = {}
+        tree.update({self.first_category: {}})
+        self.__chose_next_node(list_categories,path,df_aux,tree)
+        return tree
 
     #Treinar o modelo a partir do dataframe, é necessario que a primeira coluna contenha o atribito que se deseja prever
     def train(self,df):
@@ -93,20 +101,38 @@ class Tree():
         category,_ = sorted(entropy_gain.items(), key = lambda item: item[1], reverse = True)[0]
 
         #Adicionar elemento a arvore e a variavel path
-        self.first_atribute = category
+        self.first_category = category
         path.append(category)
         self.tree.update({category: {}})
 
-        #Selecionar os proximos nós
-        self.__chose_next_node(list_categories,path,df)
+        #Dividir o dataset entre os executors e as variaveis
+        #Inicializar variaveis
+        list_df = []
+        replicate_path = []
+        replicate_list_categories = []
+        list_trees = []
+
+        #Criar vetores para aplicar a paralelização de processos
+        for _,line in df.groupby(self.first_category):
+            list_df.append(line)
+            replicate_path.append(path)
+            replicate_list_categories.append(list_categories)
+            aux = {}
+            list_trees.append(aux)
+
+        #Selecionar os proximos nós utilizando paralelização
+        with concurrent.futures.ProcessPoolExecutor(5) as executor:
+
+            #Executar os processos em paralelo
+            results = executor.map(self.define_tree,replicate_list_categories,replicate_path,list_df)
+
+            #Concatenar os resultados obtidos
+            auxiliar_tree = {}
+            for result in results:
+                auxiliar_tree.update(result.get(self.first_category))
+            
+            #Transferir a arvore treinada para a principal
+            self.tree.get(self.first_category).update(auxiliar_tree)
 
     def get_tree(self):
         return self.tree
-
-        
-
-
-    
-
-
-    
